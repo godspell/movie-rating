83b7e3f18d29f6c5c508e860f45942bd5cef66bf
godspell <sumeethaldar731@gmail.com>
2022-07-07 00:35:55 +0530

    make changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,9 @@ import "./Header.css";
 
 const Header = () => {
   return (
-      <div className="header">
-        <div className="logo">
-            Movie App
-        </div>
-
-        <div className="user-image">
-          <img src={user} alt="user" />
-        </div>
-      </div>
+    <div className="header">
+      <div className="logo">Movie App</div>
+    </div>
   );
 };
 export default Header;
